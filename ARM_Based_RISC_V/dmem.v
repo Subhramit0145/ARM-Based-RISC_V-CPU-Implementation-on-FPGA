330d9494da425322d9ee82f789484edd8d617d95
Subhramit Bera <145998279+Subhramit0145@users.noreply.github.com>
2026-05-08 14:09:38 +0530

    Update dmem.v

--- ARM_Based_RISC_V/dmem.v
+++ ARM_Based_RISC_V/dmem.v
@@ -1,11 +1,24 @@
+
 module dmem(input  clk, we,
-			input  [31:0] a, wd,
-			output reg [31:0] rd);
-  
-	reg [31:0] RAM[63:0];
-	assign rd = RAM[a[31:2]]; // word aligned
-  
-	always@(posedge clk)
-		if (we) RAM[a[31:2]] <= wd;
-  
+            input  [31:0] a, wd,
+            output [31:0] rd);   // <-- remove 'reg' here
+
+    reg [31:0] RAM[63:0];
+
+    //DD THIS BLOCK HERE (right after RAM declaration)
+    integer i;
+    initial begin
+        for (i = 0; i < 64; i = i + 1)
+            RAM[i] = i;   // or use 32'b0 if you want all zeros
+    end
+
+    // READ
+    assign rd = RAM[a[31:2]]; 
+
+    // WRITE
+    always @(posedge clk)
+        if (we)
+            RAM[a[31:2]] <= wd;
+
 endmodule
+

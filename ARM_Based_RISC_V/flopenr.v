f5154b41d1f0a08fa68c76aca742dcf795927fae
Subhramit Bera <145998279+Subhramit0145@users.noreply.github.com>
2026-05-08 14:12:50 +0530

    Update and rename floper.v to flopenr.v

---
+++ ARM_Based_RISC_V/flopenr.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+module flopenr #(parameter WIDTH = 8)
+(
+    input clk,
+    input reset,
+    input en,
+    input [WIDTH-1:0] d,
+    output reg [WIDTH-1:0] q
+);
+
+always @(posedge clk or posedge reset)
+begin
+    if (reset)
+        q <= 0;
+    else if (en)
+        q <= d;
+end
+
+endmodule
